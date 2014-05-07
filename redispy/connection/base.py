d1f74bd723c13dd8b3137d912a49d1dcf40a9636
thomashuang <lyanghwy@gmail.com>
2014-05-07 22:28:06 +0800

    Added base socket stream classes

---
+++ redispy/connection/base.py
@@ -0,0 +1,123 @@
+import socket
+import logging
+import select
+import errno
+from redispy.net import Stream
+from redispy.errors import (
+    CannotConnectError,
+    ConnectionError,
+)
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+
+
+class Reader(object):
+
+    def __init__(self, fd, poll_timeout=1):
+        self.fd = fd
+        self.poll_timeout = poll_timeout
+
+        if hasattr(select, 'poll'):
+            self.poller = select.poll()
+            self.events = select.POLLIN | select.POLLPRI
+            self.poller.register(self.fd, self.events)
+        else:
+            self.poll_timeout = float(self.poll_timeout / 1000)
+            self.poller = None
+
+    @property
+    def readable(self):
+        if self.poller:
+            events = self.poller.poll(self.poll_timeout)
+            return bool(events)
+        else:
+            r, _, _ = select.select([self.fd], [], [], self.poll_timeout)
+            return bool(r)
+
+
+
+class RedisSocket(Stream):
+
+    READ = 0x0001
+    WRITE = 0x0004
+    ERROR = 0x0008
+    CRLF = '\r\n'
+
+    def __init__(self,  host='127.0.0.1', port=6379,
+                 path=None, ioloop=None, timeout=1):
+        super(RedisSocket, self).__init__()
+        self.host = host
+        self.port = port
+        self.path = path
+        self.timeout = timeout
+        self.connect()
+        self.sent_size = 1024
+
+    def connect(self):
+        try:
+            if self.path:
+                self.socket = self.create_unix_socket()
+            else:
+                self.socket = self.create_tcp_scoket()
+        except socket.timeout:
+            error = 'Connection to %s:%s failed: timeout' % (
+                    self.host, self.port)
+            logger.error(error)
+            raise ConnectionError(error)
+
+                # return error
+        except socket.error as error:
+            error = 'Connection to %s:%s failed: %s' % (
+                    self.host, self.port, error[0])
+            logger.error(error)
+        self._connecting = False
+        self.read_poller = Reader(self.fileno())
+
+            # return error
+    def clear(self):
+        self._in_buffer = ''
+
+    def create_unix_socket(self):
+        sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        sock.settimeout(self.timeout)
+        sock.setblocking(False)
+        sock.connect(path)
+        return sock
+
+    def create_tcp_scoket(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_TCP, socket.TCP_NODELAY, 1)
+        sock.settimeout(self.timeout)
+        sock.setblocking(False)
+        sock.connect((self.host, self.port))
+        return sock
+
+    def fileno(self):
+        return self.socket.fileno()
+
+    def disconnetc(self):
+        if self.socket:
+            self.socket.close()
+
+    def _write_to_socket(self, data):
+        sent = 0
+        if sent < len(data):
+            sent += self.socket.send(data[sent:])
+        return sent
+
+    def _read_from_socket(self, nbytes):
+        while True:
+            try: 
+                data = self.socket.recv(nbytes)
+                return data
+            except errno.EWOULDBLOCK, errno.EAGAIN:
+                if self.read_poller.readable:
+                    continue 
+            except socket.timeout:
+                return '' 
+            except socket.error as error:
+                return ''
---
+++ redispy/net.py
@@ -0,0 +1,74 @@
+
+import logging
+import socket
+
+logger = logging.getLogger(__name__)
+
+
+def create_unix_socket(path, timeout):
+    sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    sock.settimeout(timeout)
+    #sock.setblocking(False)
+    sock.connect(path)
+    return sock
+
+def create_tcp_scoket(host, port, timeout):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_TCP, socket.TCP_NODELAY, 1)
+    sock.settimeout(timeout)
+    #sock.setblocking(False)
+    sock.connect((host, port))
+    return sock
+
+
+class BaseStream(object):
+
+    CRLF = '\r\n'
+
+    def __init__(self):
+        super(BaseStream, self).__init__()
+        self._in_buffer = ''
+        self._close_callback = None
+        self._connected = False
+
+    def fileno(self):
+        raise NotImplementedError()
+
+    def close_socket(self):
+        raise NotImplementedError()
+
+    def _read_from_socket(self):
+        raise NotImplementedError()
+
+    def _write_to_socket(self, data):
+        raise NotImplementedError()
+
+
+class Stream(BaseStream):
+
+    """docstring for stream"""
+
+    def __init__(self):
+        super(Stream, self).__init__()
+
+    def gets(self):
+        crlf_index = self._in_buffer.find(self.CRLF)
+        while crlf_index == -1:
+            self._in_buffer += self._read_from_socket(1024)
+            crlf_index = self._in_buffer.find(self.CRLF)
+        i = crlf_index + len(self.__class__.CRLF)
+        result = self._in_buffer[0:i]
+        self._in_buffer = self._in_buffer[i:]
+        return result
+
+    def read(self, nbytes):
+        if len(self._in_buffer) < nbytes:
+            result = self._in_buffer
+            result += self._read_from_socket(nbytes - len(self._in_buffer))
+        else:
+            result = self._in_buffer[0:nbytes]
+            self._buffer = self._in_buffer[nbytes:]
+        return result
+
+    def write(self, data):
+        return self._write_to_socket(data)
